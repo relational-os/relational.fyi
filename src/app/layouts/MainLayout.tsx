d0bea8a6e88875bbec1294256651d186296e562b
CJ Pais <cj@cjpais.com>
2021-10-23 11:04:54 -0700

    change to linear gradient instead

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -31,37 +31,48 @@ const MainLayout = ({ children }: { children: ReactNode }) => {
             "Segoe UI Emoji", "Segoe UI Symbol";
           margin: 0;
           padding: 0;
-          background-color: #ff0000;
-          animation: bgColor 15s infinite linear;
           width: 100%;
           height: 100%;
         }
 
-        @keyframes bgColor {
-          12.5% {
-            background-color: #ff0000;
-          }
-          25% {
-            background-color: #ffa500;
-          }
-          37.5% {
-            background-color: #ffff00;
+        body {
+          background: linear-gradient(
+            238deg,
+            #fd8800,
+            #fd008f,
+            #9700fd,
+            #003dfd,
+            #05c7e6,
+            #4bd58d
+          );
+          background-size: 4800% 4800%;
+          -webkit-animation: rainbow 30s ease infinite;
+          animation: rainbow 30s ease infinite;
+        }
+
+        @-webkit-keyframes rainbow {
+          0% {
+            background-position: 0% 50%;
           }
           50% {
-            background-color: #7fff00;
-          }
-          62.5% {
-            background-color: #00ffff;
-          }
-          75% {
-            background-color: #0000ff;
-          }
-          87.5% {
-            background-color: #9932cc;
+            background-position: 100% 50%;
           }
           100% {
-            background-color: #ff1493;
+            background-position: 0% 50%;
           }
+        }
+        @keyframes rainbow {
+          0% {
+            background-position: 0% 50%;
+          }
+          50% {
+            background-position: 100% 50%;
+          }
+          100% {
+            background-position: 0% 50%;
+          }
+        }
+
         }
       `}</style>
     </div>
