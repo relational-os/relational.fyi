9d77bdb13b27f93cf10951442a830f7583f81d72
CJ Pais <cj@cjpais.com>
2021-10-23 09:40:16 -0700

    initial relational website

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -1,8 +1,15 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="main-layout">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Iceberg"
+        />
+      </head>
+
       {children}
       <style jsx>{`
         .main-layout {
@@ -12,13 +19,49 @@ const MainLayout = ({ children }: { children: ReactNode }) => {
         * {
           box-sizing: border-box;
         }
+        h1 {
+          font-size: 7rem;
+          padding: 0;
+          margin: 0;
+        }
         html,
         body {
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-            Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
-            'Segoe UI Symbol';
+          font-family: "Iceberg", -apple-system, BlinkMacSystemFont, "Segoe UI",
+            Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji",
+            "Segoe UI Emoji", "Segoe UI Symbol";
           margin: 0;
           padding: 0;
+          background-color: #ff0000;
+          animation: bgColor 15s infinite linear;
+          width: 100%;
+          height: 100%;
+        }
+
+        @keyframes bgColor {
+          12.5% {
+            background-color: #ff0000;
+          }
+          25% {
+            background-color: #ffa500;
+          }
+          37.5% {
+            background-color: #ffff00;
+          }
+          50% {
+            background-color: #7fff00;
+          }
+          62.5% {
+            background-color: #00ffff;
+          }
+          75% {
+            background-color: #0000ff;
+          }
+          87.5% {
+            background-color: #9932cc;
+          }
+          100% {
+            background-color: #ff1493;
+          }
         }
       `}</style>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const IndexPage = () => {
   return (
     <div className="index">
-      Index
+      <h1>🤝</h1>
+      <h1>RELATIONAL</h1>
+      <h3>
+        BUILDING THE FUTURE OF INTERFACES ON THE WORLD WIDE WEB THREE TODAY FOR
+        TOMORROW
+      </h3>
+      <h3>HEY@BUZZARD.LIFE</h3>
+      <h3>FOR ACTUAL INFO GO TO BUZZARD.LIFE</h3>
       <style jsx>{`
         .index {
+          margin: 10%;
         }
       `}</style>
     </div>
